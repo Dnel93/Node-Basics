882589cdd0980000fdb7232e5a234d68a9452bdb
nikedanz <dvillarreal93@gmail.com>
2020-04-19 17:48:44 -0500

    feature: monohilo

---
+++ conceptos/monohilo.js
@@ -0,0 +1,15 @@
+console.log('Hola mundo');
+
+let i = 0;
+
+setInterval(() => {
+    console.log(i);
+    i++;
+
+    if(i === 5) {
+        console.log("An error has ocurred");
+        var a = 3 + z;
+    }
+}, 1000);
+
+console.log("Tercera instrucción :)");
