4c3e2833d6c71a49a0608d11f152acab298a8bc0
nikedanz <dvillarreal93@gmail.com>
2020-05-10 19:43:16 -0500

    feature: http module

---
+++ modules/http.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+function router(req, res) {
+    console.log('Nueva petición!');
+    console.log(req.url)
+
+    switch(req.url) {
+        case '/hola':
+            res.write('Hola, que tal?');
+            res.end();
+            break;
+        default:
+            res.write('Error 404, no sé lo que quieres');
+            res.end();
+    }
+
+    // res.writeHead(201, '', {'Content-Type': 'text/plain'});
+
+    // // Escribir respuesta al usuario
+    // res.write('Hola, ya se usar HTTP de nodeJS');
+    // res.end();
+};
+
+http.createServer(router).listen(3000);
+console.log('Escuchando http en el puerto 3000')
