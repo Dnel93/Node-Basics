2fd9c295b6c91d1ae64b7024ed59e8c37f7ba8d9
nikedanz <dvillarreal93@gmail.com>
2020-05-12 20:16:15 -0500

    feature: node modules

---
+++ paquetes/modulo/index.js
@@ -0,0 +1,9 @@
+// Traer nuestro modulo
+const modulo = require('./modulo');
+
+// ejecutar una función del modulo
+// console.log(modulo);
+// modulo();
+
+console.log(modulo.prop1)
+console.log(modulo.saludar);
